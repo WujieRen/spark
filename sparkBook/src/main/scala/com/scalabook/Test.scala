161b6384668b275f4e76bbb7aec471704a022c2e
WujieRen <rwj005@outlook.com>
2018-01-29 10:36:46 +0800

    SparkBoot Test : A WordCount exam

--- sparkBook/src/main/scala/com/scalabook/Test.scala
+++ sparkBook/src/main/scala/com/scalabook/Test.scala
@@ -1,8 +1,21 @@
 package com.scalabook
 
+import org.apache.spark.sql.SparkSession
+
 /**
   * Created by renwujie on 2018/01/15 at 20:17
+  *
+  * 1、获取数据，看下啥样
   */
 object Test {
+  def main(args: Array[String]): Unit = {
 
+    val spark = SparkSession.builder().master("local[*]").appName("Test").getOrCreate()
+
+    val srcData = spark.read.csv("hdfs://testenv:8020/testdata/spark/reilly/linkage")
+
+    println(srcData.first().toString())
+
+    spark.close()
+  }
 }
